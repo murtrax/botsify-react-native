b6b56b964ba0139d81e62e27498f43e0eb5453db
Hassaan Ahmed <hahmed.bscs16seecs@seecs.edu.pk>
2019-06-28 14:24:56 +0500

    Created the page where you view your current bots.

--- NewApp/src/components/Home/index.js
+++ NewApp/src/components/Home/index.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
 import {
   Header, Left, Container, Button, Body, Title, Right, Icon, Text, Content } from "native-base";
-import { StatusBar } from "react-native";
+import { StatusBar , View, Image} from "react-native";
 import styles from "./styles";
+
+const avatar = require("../../../assets/avatar1.png");
 
 export default class Home extends Component {
   render() {
@@ -24,8 +26,54 @@ export default class Home extends Component {
           </Body>
           <Right style={styles.headerRight}/>
         </Header>
-        <Content>
-          <Text style={{ alignSelf: "center", marginTop: 10}}>Botsify App</Text>
+
+
+
+
+        <Content style={{backgroundColor: "white"}}>
+
+        <View style={{flex: 1, alignSelf: "stretch"}}>
+
+          <View style={styles.BotsifyHeader}>
+          <Text style={styles.BotsifyHeaderText}>Botsify</Text>
+          </View>
+
+          <View style={styles.BotsSection}>
+
+            <View  style={{flex:1, backgroundColor: "#7CD1E9"}}>
+              <View style={styles.BotsifyHeaderRadius}>
+            <Text style={styles.YourBotText}>Your Bots</Text>
+             </View>
+            </View>
+
+            <View style={{flex:9}}>
+
+              <View style={styles.BotViewHorizontal}>
+                <View style={styles.BotViewLeft}>
+                <Image source={avatar} style = {styles.BotImage}/>
+              <Text style={styles.BotNameText}>Bot 1</Text>
+               </View>
+               <View style={styles.BotViewRight}>
+                <Image source={avatar} style = {styles.BotImage}/>
+              <Text style={styles.BotNameText}>Bot 2</Text>
+               </View>
+              </View>
+
+              <View style={styles.BotViewHorizontal}>
+              <View style={styles.BotViewLeft}>
+                <Image source={avatar} style = {styles.BotImage}/>
+              <Text style={styles.BotNameText}>Bot 3</Text>
+               </View>
+               <View style={styles.BotViewRight}>
+                <Image source={avatar} style = {styles.BotImage}/>
+              <Text style={styles.BotNameText}>Bot 4</Text>
+               </View>
+              </View>
+
+            </View>
+          </View>
+
+        </View>
         </Content>
       </Container>
     );
--- NewApp/src/components/Home/styles.js
+++ NewApp/src/components/Home/styles.js
@@ -1,18 +1,107 @@
 export default {
   container: {
-    flex: 1,
-    backgroundColor: "#fff"
+    flex: 1
   },
   headerLeft: {
-    flex: 0.3
+    flex: 0.3,
   },
   headerBody: {
-    flex: 0.4
+    flex: 0.4,
   },
   textBody:{
-    alignSelf: "center"
+    alignSelf: "center",
   },
   headerRight: {
-    flex: 0.3
+    flex: 0.3,
+  },
+
+  BotsifyHeaderText: {
+    alignSelf: "center",
+    marginTop: 10,
+    marginBottom: 10,
+    fontSize: 27,
+    color: "white",
+    fontWeight: "bold",
+  },
+
+  BotsifyHeader: {
+    flex: 1,
+    backgroundColor: "#7CD1E9",
+  },
+
+  BotsSection:{
+    flex: 9,
+    borderRadius: 14,
+    borderTop: 12,
+    backgroundColor: "white",
+    alignSelf: "stretch",
+    //flexDirection: "column",
+
+  },
+
+  YourBotText:{
+    alignSelf: "center",
+    marginTop: 15,
+    marginBottom: 15,
+    fontSize: 27,
+    fontWeight: "bold",
+  },
+
+  BotsifyHeaderRadius:{
+    flex: 1, 
+    borderTopLeftRadius: 19,
+    borderTopRightRadius: 19, 
+    backgroundColor: "#FFFFFF",
+  },
+
+  BotViewHorizontal:{
+    marginLeft: 20,
+    marginRight: 20,
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+
+  BotViewLeft:{
+    borderRadius: 8,
+    backgroundColor: "white",
+    borderColor: "black",
+    borderWidth: 1,
+    flex: 1,
+    marginBottom: 22,
+    marginLeft: 22,
+    marginTop: 22,
+    marginRight: 16,
+    alignItems: "center",
+  },
+
+  BotViewRight:{
+    borderRadius: 8,
+    backgroundColor: "white",
+    borderColor: "black",
+    borderWidth: 1,
+    flex: 1,
+    marginBottom: 22,
+    marginLeft: 16,
+    marginTop: 22,
+    marginRight: 22,
+    alignItems: "center",
+  },
+
+  BotNameText:{
+    alignSelf: "center",
+    alignItems: "center",
+    marginTop: 10,
+    fontSize: 15,
+    flexDirection: "column",
+    marginBottom: 10,
+    fontWeight: "bold",
+  },
+
+  BotImage:
+  {
+      alignSelf: "center",
+      marginTop: 10,
+      marginBottom: 5,
   }
 };
